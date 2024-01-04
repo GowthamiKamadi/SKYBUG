9dd9ae1e18b754c933864ad0ed4d7daa3aa59faa
GowthamiKamadi <154670333+GowthamiKamadi@users.noreply.github.com>
2024-01-04 14:38:39 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,11 @@
+#TASK-1:- creating a simple command line calculator using python
+#taking input from the user
 A = int(input("Enter A value:  "))
 B = int(input("Enter B value:  "))
 print("WELCOME TO SKYBUG CALCULATOR....😄")
+#mentioning operations to be performed
 operation = input('select one operation to perform(\n 1.ADDITION(+)\n 2.Subtraction(-)\n 3.Division(/)\n 4.Multiplication(*)\n 5.Power(**)\n 6.Floor division(//)👾:  ')
+#instructions of how to be calculated
 if (operation == '1' ):
     print ('The Answer is: ',A+B)
 elif (operation == '2' ):
